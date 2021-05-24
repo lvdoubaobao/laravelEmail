ddb2d879c3dbbf2525cd7feea7f9170ef160f59b
绿豆包包 <736400469@qq.com>
2021-05-24 09:23:54 +0800

    上传excel

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-$mail=new \App\Reoisitory\MailGunRespository();
-$mail->to('736400469@qq.com');
 
-$mail->send('aa','aa');
-//return redirect()->to('/admin');
+return redirect()->to('/admin');
 });
